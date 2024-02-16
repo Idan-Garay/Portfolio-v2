6e50e26443cdeb6748c1aec442556c7a42a45787
Idan <garayidan@gmail.com>
2024-02-16 23:51:08 +0800

    fix: add transition to contact section

--- app/components/contact-section/index.js
+++ app/components/contact-section/index.js
@@ -2,21 +2,38 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "./index.module.css";
 import { roboto } from "@/app/fonts";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 
 export const ContactSection = () => {
+  const ref = useRef();
+  const isInView = useInView(ref, { once: true });
+  const animationStyle = {
+    transition: "all 0.8s",
+    opacity: isInView ? 1 : 0,
+    transform: isInView ? "none" : "translateX(150px)",
+  };
   return (
     <section id="contact" className={styles.contactSection}>
-      <div className={styles.sectionContent}>
-        <h1 style={roboto.style} className={styles.h1}>
+      <div className={styles.sectionContent} ref={ref}>
+        <h1
+          style={{ ...roboto.style, ...animationStyle }}
+          className={styles.h1}
+        >
           Let&apos;s Connect
         </h1>
-        <p>
+        <p style={animationStyle}>
           Feel free to reach out if you’re looking for a developer, have a
           question, or just want to connect.
         </p>
         <div className={styles.label}>
-          <div className={styles.labelLine}></div>
-          <b className={styles.p}>garayidan@gmail.com</b>
+          <div style={animationStyle} className={styles.labelLine}></div>
+          <b
+            style={{ ...animationStyle, transitionDelay: "0.8s" }}
+            className={styles.p}
+          >
+            garayidan@gmail.com
+          </b>
         </div>
       </div>
       <footer className={styles.footer}>
@@ -28,7 +45,7 @@ export const ContactSection = () => {
             position: "relative",
           }}
         >
-          <Link href="#heroSection">
+          <Link  href="#heroSection">
             <Image
               src="/white-logo.svg"
               alt="logo"
@@ -38,21 +55,21 @@ export const ContactSection = () => {
           </Link>
         </div>
         <ul>
-          <Link
+          <Link style={{ ...animationStyle, transitionDelay: "0.5s" }}
             target="_blank"
             className={styles.link}
             href="https://www.linkedin.com/in/idan-joshua-garay"
           >
             Linkedin
           </Link>
-          <Link
+          <Link style={{ ...animationStyle, transitionDelay: "0.6s" }}
             target="_blank"
             className={styles.link}
             href="https://github.com/Idan-Garay"
           >
             GitHub
           </Link>
-          <Link
+          <Link style={{ ...animationStyle, transitionDelay: "0.7s" }}
             target="_blank"
             className={styles.link}
             href="https://twitter.com/GarayIdan"
