40a3850aeaf03fbb117aa4d9d751e4aba1242f1c
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2024-05-23 17:30:34 +0800

    Update index.js

--- app/components/about-section/index.js
+++ app/components/about-section/index.js
@@ -40,7 +40,7 @@ export const AboutSection = () => {
             </h5>
           </div>
           <p className={styles.p} style={rightToLeft}>
-            Having a years of work experience as a front-end, and full-stack
+            I have 2 years of work experience as a front-end, and full-stack
             developer
           </p>
 
